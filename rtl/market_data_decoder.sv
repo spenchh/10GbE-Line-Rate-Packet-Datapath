13450d0bb3bc4339a8e0ef8fa55b626104d41a89
spenchh <kogomaspencer@gmail.com>
2026-08-30 10:25:59 -0500

    interface

--- rtl/market_data_decoder.sv
+++ rtl/market_data_decoder.sv
@@ -1,15 +1,17 @@
 module market_data_decoder (
-  input clk,
-  input rst,
+  input logic clk,
+  input logic rst_n,
+  input logic packet_valid,
+  input logic [247:0] packet_in,
 
-  
-  input [7:0] message_type,
-  input [31:0] sequence_number,
-  input [15:0] symbol_id,
-  input [31:0] bid_price,
-  input [31:0] ask_price,
-  input [31:0] bid_size,
-  input [31:0] ask_size,
-  input [63:0] timestamp 
+  output logic decoded_valid,
+  output logic [7:0] message_type,
+  output logic [31:0] sequence_number,
+  output logic [15:0] symbol_id,
+  output logic [31:0] bid_price,
+  output logic [31:0] ask_price,
+  output logic [31:0] bid_size,
+  output logic [31:0] ask_size,
+  output logic [63:0] timestamp 
 );
 
